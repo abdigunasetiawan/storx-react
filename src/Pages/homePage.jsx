77ccd452d4b2dbcd1f042b239033515a1e444f5d
abdigunasetiawan <abdigunasetiawan0@gmail.com>
2025-02-20 21:51:10 +0700

    menambahkan section footer pada home page

--- src/Pages/homePage.jsx
+++ src/Pages/homePage.jsx
@@ -3,6 +3,7 @@ import BestProducts from "../components/BestProducts";
 import Category from "../components/Category";
 import Jumbotron from "../components/Jumbotron";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const HomePage = () => {
   return (
@@ -12,6 +13,7 @@ const HomePage = () => {
       <BestProducts></BestProducts>
       <Category></Category>
       <AllProducts></AllProducts>
+      <Footer></Footer>
     </div>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer class="">
+      <div class="border px-4 py-4">
+        <p class="text-center">
+          &copy; 2024. built by <span class="font-bold">Abdi</span>&#128561;. made with stress&#128545;
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
